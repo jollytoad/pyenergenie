4477405b5d819f76bbb9b3ea3534d0072c96abfd
Mark Gibson <jollytoad@gmail.com>
2016-10-04 14:40:04 +0100

    Updates to etrv_stream

--- src/etrv_stream.py
+++ src/etrv_stream.py
@@ -2,33 +2,82 @@ from __future__ import print_function
 import thread
 import Queue
 import json
-import energenie
-import energenie.OnAir
-import energenie.OpenThings
+import time
+import energenie.radio as radio
+import energenie.OnAir as OnAir
+import energenie.OpenThings as OpenThings
 import sys
 
-readin = raw_input
+CRYPT_PID = 242
+
 q = Queue.Queue()
-air_interface = energenie.OnAir.OpenThingsAirInterface()
+air_interface = OnAir.OpenThingsAirInterface()
 
-energenie.init()
+def init():
+    print("radio.init", file=sys.stderr)
+    radio.init()
 
-def incoming(address, message):
-    # Only eTRV messages
-    if address[1] == 3:
-        # debugging: dump encoded message to stderr
-        print("RX:", energenie.OpenThings.encode(message), file=sys.stderr)
+    print("OpenThings.init", file=sys.stderr)
+    OpenThings.init(CRYPT_PID)
 
-        # write JSON representation of an OpenThings message to stdout
+    print("thread.start_new_thread", file=sys.stderr)
+    thread.start_new_thread(read_from_stdin, ())
+
+def loop():
+    print("loop", file=sys.stderr)
+    while True:
+        message = listen_to_radio()
+        write_message(message)
+        transmit_next_queued_message()
+
+def might_be_etrv(payload):
+    return payload[1] == 4 and payload[2] == 3
+
+# write JSON representation of an OpenThings message to stdout
+def write_message(message):
+    if message:
         sys.stdout.write(json.dumps(message.pydict))
         sys.stdout.write("\n")
         sys.stdout.flush()
 
-def poller():
+def listen_to_radio(receive_time=1):
+    print("listen_to_radio", file=sys.stderr)
+    radio.receiver(fsk=True)
+    now = time.time()
+    timeout = now + receive_time
+
+    while now < timeout:
+        if radio.is_receive_waiting():
+            payload = radio.receive_cbp()
+            now = time.time()
+
+            if might_be_etrv(payload):
+                # debugging: dump encoded message to stderr
+                print("RX:", payload, file=sys.stderr)
+
+                try:
+                    return OpenThings.decode(payload, receive_timestamp=now)
+                except OpenThings.OpenThingsException:
+                    pass
+
+        now = time.time()
+
+    return None
+
+def transmit_next_queued_message():
+    if not q.empty():
+        # take top message from the queue and transmit it
+        message = q.get()
+        air_interface.send(message)
+
+        # debugging: dump encoded message to stderr
+        print("TX:", OpenThings.encode(message), file=sys.stderr)
+
+def read_from_stdin():
     while True:
         try:
             # parse JSON representation of an OpenThings message
-            message = energenie.OpenThings.Message(pydict=json.loads(readin()))
+            message = OpenThings.Message(pydict=json.loads(raw_input()))
 
             # add it to the queue
             q.put(message)
@@ -36,20 +85,8 @@ def poller():
             # TODO: log errors to stderr
             pass
 
-in_thread = thread.start_new_thread(poller, ())
-energenie.fsk_router.when_incoming(incoming)
-
-while True:
-    energenie.loop()
-
-    if not q.empty():
-        # take top message from the queue and transmit it
-        message = q.get()
-        air_interface.send(message)
-
-        # debugging: dump encoded message to stderr
-        print("TX:", energenie.OpenThings.encode(message), file=sys.stderr)
-
+init()
+loop()
 
 # Test msg for stdin:
 #{"header": {"sensorid": 2003, "productid": 3, "mfrid": 4}, "recs": [{"wr": true, "paramid": 116, "typeid": 144, "length": 2, "value": 15.0}]}
